b4ff3d7d99346cd68545b5538ed4104495a79d6e
Sharad <ksfbd100@gmail.com>
2026-09-05 22:32:09 +0530

    Patterns printing

--- src/Basics/Patterns/Pattern1.java
+++ src/Basics/Patterns/Pattern1.java
@@ -5,6 +5,9 @@ public class Pattern1 {
 //        rectanglePattern();
 //        rightPyramid();
 //        numericRightPyramid();
+//        uniNumRightPyr();
+//        upperRightPyr();
+//        pyramid();
     }
 
     static void rectanglePattern(){
@@ -36,10 +39,42 @@ public class Pattern1 {
 
     static void uniNumRightPyr(){
         for (int i=1; i<=5; i++){
-            for (int j=0; j<=i; j++){
+            for (int j=1; j<=i; j++){
                 System.out.print(i+" ");
             }
             System.out.println();
+        }
+    }
+
+    static void upperRightPyr(){
+        for (int i=5; i>=1; i--){
+            for (int j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pyramid(){
+        for (int i=0; i<=1; i++){
+            for (int j=0; j<=i; j++){
+                System.out.print(" "+"*"+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void rightUppPllgrm(int n){
+        for (int i=1; i<=n; i++){
+            for (int j=(2*i)+2; j>=0; j--){
+                System.out.println(" ");
+            }
+            for (int j=1; j<=5; j++){
+                System.out.println("*");
+            }
+            for (int j=0; j<(2*i)-2; j++){
+                System.out.println(" ");
+            }
         }
     }
 }
